cd69c822171be6c6808766595c6f11b3f7c02325
Blake Wyatt <nabakin@protonmail.ch>
2020-02-01 15:58:22 -0500

    Add additional image targets and Auto Focus

---
+++ Assets/Scripts/AutoFocus.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class AutoFocus : MonoBehaviour
+{
+  void Start ()
+  {
+    var vuforia = VuforiaARController.Instance;
+    vuforia.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+    vuforia.RegisterOnPauseCallback(OnPaused);
+  }
+
+  private void OnVuforiaStarted()
+  {
+    bool focusModeSet = CameraDevice.Instance.SetFocusMode(
+        CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+
+      if (!focusModeSet) {
+        Debug.Log("Failed to set focus mode (unsupported mode).");
+      }
+  }
+
+  private void OnPaused(bool paused)
+  {
+    if (!paused) // resumed
+    {
+        // Set again autofocus mode when app is resumed
+        CameraDevice.Instance.SetFocusMode(
+            CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+    }
+  }
+}
