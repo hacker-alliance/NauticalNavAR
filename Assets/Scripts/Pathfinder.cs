b587732a38a01268fafbd994347c7bf3f884cbd8
joseiciano <icianojose12345@aol.com>
2020-02-01 17:09:34 -0500

    fixing needed dilboi

---
+++ Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Pathfinder : DefaultTrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+
+   void OnTrackableStateChange(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+ 
+        Debug.Log("test");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour) { mTrackableBehaviour.RegisterOnTrackableStatusChanged(this); }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
